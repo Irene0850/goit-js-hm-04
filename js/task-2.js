60c2572fb5f68e6a873bd3f021a5eecc81fc1b4b
Iryna Kolomiichenko <159377221+Irene0850@users.noreply.github.com>
2024-06-09 14:33:53 +0300

    new js

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,14 @@
-function calcAverageCalories(days) {}
+function calcAverageCalories(days) {
+  let totalCalories = 0;
+  for (const day of days) {
+    totalCalories += day.calories;
+  }
+  if (days.length === 0) {
+    return 0;
+  }
+  const averageCalories = totalCalories / days.length;
+  return averageCalories;
+}
 
 console.log(
   calcAverageCalories([
